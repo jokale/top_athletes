4f009008c930e1e3012a436b01ae4a4efb803947
Joanna E K <kalemajoanna@gmail.com>
2020-01-12 19:26:45 +0000

    changed new puts statement

--- lib/top_athletes/cli.rb
+++ lib/top_athletes/cli.rb
@@ -1,7 +1,7 @@
 class CLI 
   def start  
     puts "Hey there and welcome to the top athletes of the century!"
-    puts "Pick a number from 1 to 100 to see which athletes rank where!"
+    puts "Pick a number from 1 to 100 to see which athletes rank where in 2019"
   end 
   
 end 
