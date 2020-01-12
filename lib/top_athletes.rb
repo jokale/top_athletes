fc71141229c7f80748129658dbf67ede761ac42b
Joanna E K <kalemajoanna@gmail.com>
2020-01-12 18:01:50 +0000

    first commit

---
+++ lib/top_athletes.rb
@@ -0,0 +1,6 @@
+require "top_athletes/version"
+
+module TopAthletes
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/top_athletes/version.rb
@@ -0,0 +1,3 @@
+module TopAthletes
+  VERSION = "0.1.0"
+end
