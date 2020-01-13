c88126865abbdb72fa9bfce8a0790b4b77da0d62
Joanna E K <kalemajoanna@gmail.com>
2020-01-13 10:26:33 +0000

    found the css variable

--- lib/top_athletes/scraper.rb
+++ lib/top_athletes/scraper.rb
@@ -1,10 +1,13 @@
 class Scraper 
   
   def self.scrape_athletes 
-    page = Nokogiri::HTML(open("http://www.espn.com/espn/feature/story/_/id/26113613/espn-world-fame-100-2019")) 
+    page = Nokogiri::HTML(open("https://www.businessinsider.com/iconic-female-athletes-women-sports-2019-7?r=US&IR=T")) 
     
     binding.pry 
     
+    page.css(".slide-title-text").each do |athletes|
+      
   end 
   
 end 
+<h2 class="slide-title-text">Billie Jean King</h2>
