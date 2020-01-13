fe7eb2a80437c4bbddb6eb6225c0f2e6a1721237
Joanna E K <kalemajoanna@gmail.com>
2020-01-13 09:06:10 +0000

    added scraper to cli

--- lib/top_athletes/cli.rb
+++ lib/top_athletes/cli.rb
@@ -2,6 +2,7 @@ class CLI
   def start  
     puts "Hey there and welcome to the top athletes of the century!"
     puts "Pick a number from 1 to 100 to see which athletes rank where in 2019"
+    Scraper.scrape_athletes
   end 
   
 end 
