6d6d5eccaad337f2d08e2fd5198d230eb39d9f51
Joanna E K <kalemajoanna@gmail.com>
2020-01-13 09:16:21 +0000

    required scraper file

--- lib/top_athletes.rb
+++ lib/top_athletes.rb
@@ -2,6 +2,7 @@ require "nokogiri"
 require "open-uri"
 require_relative "./top_athletes/version"
 require_relative "./top_athletes/cli"
+require_relative "./top_athletes/scraper"
 
 module TopAthletes
   class Error < StandardError; end
