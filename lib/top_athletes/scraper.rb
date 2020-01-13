ec0a6848f1963b17b23558404d5ccace500b064d
Joanna E K <kalemajoanna@gmail.com>
2020-01-13 22:24:40 +0000

    added css

--- lib/top_athletes/scraper.rb
+++ lib/top_athletes/scraper.rb
@@ -3,14 +3,15 @@ class Scraper
   def self.scrape_athletes 
     page = Nokogiri::HTML(open("https://www.businessinsider.com/iconic-female-athletes-women-sports-2019-7?r=US&IR=T")
    
-   page.css(".slide-title-text").each do |top_athlete ,  index|
+   page.css(".slide-title-text").each_with_index do |top_athlete ,  index|
+     
+     
   title = page.css(".slide-title-text")[index].text
   
- athlete = Athlete.new(title)
+athlete = Athlete.new(title)
    
 binding.pry 
 
-  end 
-
-  end 
+ end 
+ end 
 end 
