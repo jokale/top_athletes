eeaa1d3fc87c3972f693f154cc885aed3d35bead
Joanna E K <kalemajoanna@gmail.com>
2020-01-13 10:27:06 +0000

    added css

--- lib/top_athletes/scraper.rb
+++ lib/top_athletes/scraper.rb
@@ -3,11 +3,11 @@ class Scraper
   def self.scrape_athletes 
     page = Nokogiri::HTML(open("https://www.businessinsider.com/iconic-female-athletes-women-sports-2019-7?r=US&IR=T")) 
     
-    binding.pry 
+    
     
     page.css(".slide-title-text").each do |athletes|
       
+      binding.pry 
   end 
   
 end 
-<h2 class="slide-title-text">Billie Jean King</h2>
