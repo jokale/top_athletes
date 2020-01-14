a8a3bdc0289f4ce13a0ce6f3ae737bc8509381c9
Joanna E K <kalemajoanna@gmail.com>
2020-01-14 09:00:44 +0000

    removed pry

--- lib/top_athletes/scraper.rb
+++ lib/top_athletes/scraper.rb
@@ -1,17 +1,15 @@
 class Scraper 
   
   def self.scrape_athletes 
-    page = Nokogiri::HTML(open("https://www.businessinsider.com/iconic-female-athletes-women-sports-2019-7?r=US&IR=T")
+    page = Nokogiri::HTML(open("https://www.businessinsider.com/iconic-female-athletes-women-sports-2019-7?r=US&IR=T"))
    
-   page.css(".slide-title-text").each_with_index { |top_athlete ,  index|}
+   page.css(".slide-title-text").each_with_index  do  |top_athlete, index|
+     title = page.css(".slide-title-text")[index].text
      
-     
-  title = page.css(".slide-title-text")[index].text
-  
-athlete = Athlete.new(title)
+     athlete = Athletes.new(title)
    
-binding.pry 
+ 
+end 
 
- end 
  end 
 end 
