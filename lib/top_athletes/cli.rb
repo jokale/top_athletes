1b05e95fa9023a8aee727a5ad351b48f88ced498
Joanna E K <kalemajoanna@gmail.com>
2020-01-15 17:06:45 +0000

    add changes to cli and comments

--- lib/top_athletes/cli.rb
+++ lib/top_athletes/cli.rb
@@ -1,11 +1,16 @@
 class CLI 
   def start  
-    puts "Hey there and welcome to the 36 most iconic female athletes of the century!"
-    puts "Pick an athelete to see more details about the athlete"
+    puts "Hey there and welcome to the 36 most iconic female athletes of the century! Below you'll see a list of top athletes as per Business Insider"
+    puts "Pick an athelete to see a short summary of the athlete"
     Scraper.scrape_athletes
     
     athletes = Athletes.all 
     athletes.each.with_index(1) do |athletes, index| puts "#{index}.#{athletes.name}" 
+    
+    
+    #I want the user to be able to pick the index number or even name of athlete then they are able to get information sbout the information
+    # I also want to create a range if possible to print only number 1 - 36 and if possible have a url for the most paid athletes 
+    
 
     end 
   end 
