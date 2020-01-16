2c6b66fbfd22b48443a00d563e3c9a6f17bfba74
Joanna E K <kalemajoanna@gmail.com>
2020-01-16 21:38:50 +0000

    removed commented out scraper notes

--- lib/top_athletes/scraper.rb
+++ lib/top_athletes/scraper.rb
@@ -1,38 +1,17 @@
 class Scraper 
-  
-#   def self.scrape_athletes 
-#     page = Nokogiri::HTML(open("https://www.businessinsider.com/iconic-female-athletes-women-sports-2019-7?r=US&IR=T"))
-   
-#   page.css(".slide-title-text").each_with_index  do  |top_athlete, index|
-#     name = page.css(".slide-title-text")[index].text
-     
-#     athlete = Athletes.new(name)
-    
-   
-# end 
-# end 
 
-# def self.scrape_description 
-#   page = Nokogiri::HTML(open("https://www.businessinsider.com/iconic-female-athletes-women-sports-2019-7?r=US&IR=T"))
-#   page.css("div.slide-layout.clearfix p").each_with_index do |athlete_description, index|
-# description = page.css("div.slide-layout.clearfix p")[index].text
- 
-
-# # athlete_description = Athletes.new(name, description)
-# end 
-# end 
-# end 
 
 def self.scrape_athletes 
-page = Nokogiri::HTML(open("https://www.businessinsider.com/iconic-female-athletes-women-sports-2019-7?r=US&IR=T"))
-page.css(".slide-title-text").each_with_index  do  |top_athlete, index|
-name = page.css(".slide-title-text")[index].text
-athlete = Athletes.new(name)
-end 
+  page = Nokogiri::HTML(open("https://www.businessinsider.com/iconic-female-athletes-women-sports-2019-7?r=US&IR=T"))
+  page.css(".slide-title-text").each_with_index  do  |top_athlete, index|
+  name = page.css(".slide-title-text")[index].text
+  
+  athlete = Athletes.new(name)
+   end 
  end 
 
 def self.scrape_description
-page = Nokogiri::HTML(open("https://www.businessinsider.com/iconic-female-athletes-women-sports-2019-7?r=US&IR=T"))
+ page = Nokogiri::HTML(open("https://www.businessinsider.com/iconic-female-athletes-women-sports-2019-7?r=US&IR=T"))
 description = page.css("div.slide-layout.clearfix p").index.text
 athlete_description = Athletes.new(description)
 end
