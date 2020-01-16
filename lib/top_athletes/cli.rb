b8a4b8837067e1ba16ea663fe283ad7535f60084
Joanna E K <kalemajoanna@gmail.com>
2020-01-16 11:22:41 +0000

    changes to cli

--- lib/top_athletes/cli.rb
+++ lib/top_athletes/cli.rb
@@ -1,12 +1,6 @@
 class CLI 
   def start  
     puts "Hey there and welcome to the 36 most iconic female athletes of the century as per Business Insider"
-    # puts "Below you'll see a list of top athletes as per Business Insider"
-    # puts ""
-    # puts "Want to know more about a specific athlete on the list?"
-    # puts ""
-    # puts "Pick any number from 1-36 to see a short summary of the athlete: their orgin and the sport they play!"
-    # puts ""
     Scraper.scrape_athletes
     menu
   end 
@@ -29,6 +23,21 @@ class CLI
     if input.to_i.between?(1, Athletes.all.size)
         index = input.to_i - 1
 
+athletes = Athletes.all[index]
+        Scraper.scrape_athletes(athletes)
+        display_details(athletes)
+      else
+        puts "Goodbye, hope you have a great day!"
+      end
+    end
+  end
+
+  def display_details(athletes)
+    puts ""
+    puts "description: #{athletes.description}"
+  end
+
+end
     
     
     #I want the user to be able to pick the index number or even name of athlete then they are able to get information sbout the information
