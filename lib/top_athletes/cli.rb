261b72dd0ecfbf286458721bce724ece2c278d28
jokale <kalemajoanna@gmail.com>
2020-02-02 23:05:39 +0000

    fixed the description method where now description matches athlete index

--- lib/top_athletes/cli.rb
+++ lib/top_athletes/cli.rb
@@ -1,12 +1,9 @@
-require "pry"
-
 class CLI 
   
   def start  
     puts "Hey there and welcome to the 36 most iconic female athletes of the century as per Business Insider"
     Scraper.scrape_athletes
     menu
-    Scraper.scrape_description
   end 
   
   def menu 
@@ -41,27 +38,162 @@ end
    
 athletes = Athletes.all[index]
   
-        display_details(athletes)
+        display_details(index+ 1)
       else
         puts "Goodbye, hope you have a great day! Who knows maybe you'll be on this list soon!"
       end
     end
   end
 
-  def display_details(athletes)
-    puts ""
-    description = Scraper.scrape_description.text.strip 
-    
-        
-     puts  "Athlete description: #{description}"
+  def display_details(input)
+    if input == 1 
+      puts first_description = Scraper.scrape_description[0..2].text.strip 
+    elsif input == 2
+      puts second_description = Scraper.scrape_description[3..5].text.strip 
+      puts ""
+      puts "Would you like to find out more about another athlete?"
+    elsif input == 3
+      puts third_description = Scraper.scrape_description[6..9].text.strip
+      puts ""
+      puts "Would you like to find out more about another athlete?"
+    elsif input == 4
+      puts fourth_description = Scraper.scrape_description[10..11].text.strip
+      puts ""
+      puts "Would you like to find out more about another athlete?"
+    elsif input == 5
+      puts fifth_description = Scraper.scrape_description[12..15].text.strip
+      puts ""
+      puts "Would you like to find out more about another athlete?"
+    elsif input == 6
+      puts sixth_description = Scraper.scrape_description[16..18].text.strip
+      puts ""
+      puts "Would you like to find out more about another athlete?"
+    elsif input == 7
+      puts seventh_description = Scraper.scrape_description[19..23].text.strip
+      puts ""
+      puts "Would you like to find out more about another athlete?"
+    elsif input == 8
+      puts eigth_description = Scraper.scrape_description[24..27].text.strip
+      puts ""
+      puts "Would you like to find out more about another athlete?"
+    elsif input == 9
+      puts ninth_description =Scraper.scrape_description[28..32].text.strip
+      puts ""
+      puts "Would you like to find out more about another athlete?"
+    elsif input == 10
+      puts tenth_description = Scraper.scrape_description[33..35].text.strip
+      puts ""
+      puts "Would you like to find out more about another athlete?"
+    elsif input == 11
+      puts eleventh_description = Scraper.scrape_description[36..38].text.strip
+      puts ""
+      puts "Would you like to find out more about another athlete?"
+    elsif input == 12
+      puts twelvth_description = Scraper.scrape_description[39..43].text.strip
+      puts ""
+      puts "Would you like to find out more about another athlete?"
+    elsif input == 13
+      puts thirteenth_description = Scraper.scrape_description[44..47].text.strip
+      puts ""
+      puts "Would you like to find out more about another athlete?"
+    elsif input == 14
+      puts fourteenth_description = Scraper.scrape_description[48..51].text.strip
+      puts ""
+      puts "Would you like to find out more about another athlete?"
+    elsif input == 15
+      puts fifteenth_description = Scraper.scrape_description[52..56].text.strip 
+      puts ""
+      puts "Would you like to find out more about another athlete?"
+    elsif input == 16
+      puts sixteenth_description = Scraper.scrape_description[57..60].text.strip
+      puts ""
+      puts "Would you like to find out more about another athlete?"
+    elsif input == 17
+      puts seventeenth_description = Scraper.scrape_description[61..63].text.strip
+      puts ""
+      puts "Would you like to find out more about another athlete?"
+    elsif input == 18
+      puts eighteenth_description = Scraper.scrape_description[64..65].text.strip
+      puts ""
+      puts "Would you like to find out more about another athlete?"
+    elsif input == 19
+      puts nineteenth_description = Scraper.scrape_description[66..68].text.strip
+      puts ""
+      puts "Would you like to find out more about another athlete?"
+    elsif input == 20
+      puts twentieth_description = Scraper.scrape_description[69..72].text.strip
+      puts ""
+      puts "Would you like to find out more about another athlete?"
+    elsif input == 21
+      puts twenty_first_description = Scraper.scrape_description[73..75].text.strip
+      puts ""
+      puts "Would you like to find out more about another athlete?"
+    elsif input == 22
+      puts twenty_second_description = Scraper.scrape_description[76..80].text.strip
+      puts ""
+      puts "Would you like to find out more about another athlete?"
+    elsif input == 23
+      puts  twenty_third_description = Scraper.scrape_description[81..82].text.strip
+      puts ""
+      puts "Would you like to find out more about another athlete?"
+    elsif input == 24
+      puts twenty_fourth_description = Scraper.scrape_description[83..85].text.strip
+      puts ""
+      puts "Would you like to find out more about another athlete?"
+    elsif input == 25
+      puts twenty_fifth_description = Scraper.scrape_description[86..89].text.strip
+      puts ""
+      puts "Would you like to find out more about another athlete?"
+    elsif input == 26
+      puts twenty_sixth_description = Scraper.scrape_description[90..93].text.strip
+      puts ""
+      puts "Would you like to find out more about another athlete?"
+    elsif input == 27
+      puts twenty_seventh_description = Scraper.scrape_description[94..98].text.strip
+      puts ""
+      puts "Would you like to find out more about another athlete?"
+    elsif input == 28
+      puts  twenty_eigth_description = Scraper.scrape_description[99..101].text.strip
+      puts ""
+      puts "Would you like to find out more about another athlete?"
+    elsif input == 29
+      puts twenty_ninth_description = Scraper.scrape_description[102..103].text.strip
+      puts ""
+      puts "Would you like to find out more about another athlete?"
+    elsif input == 30
+      puts thirtieth_description = Scraper.scrape_description[104..107].text.strip
+      puts ""
+      puts "Would you like to find out more about another athlete?"
+    elsif input == 31
+      puts  thirty_one_description = Scraper.scrape_description[108..110].text.strip
+      puts ""
+      puts "Would you like to find out more about another athlete?"
+    elsif input == 32
+      puts thirty_two_description = Scraper.scrape_description[111..115].text.strip
+      puts ""
+      puts "Would you like to find out more about another athlete?"
+    elsif input == 33
+      puts thirty_three_description = Scraper.scrape_description[116..118].text.strip
+      puts ""
+      puts "Would you like to find out more about another athlete?"
+    elsif input == 34
+      puts thirty_four_description = Scraper.scrape_description[119..122].text.strip
+      puts ""
+      puts "Would you like to find out more about another athlete?"
+    elsif input == 35
+      puts thirty_five_description = Scraper.scrape_description[123..125].text.strip
+      puts ""
+      puts "Would you like to find out more about another athlete?"
+    elsif input == 36
+      puts thirty_sixth_description = Scraper.scrape_description[126..127].text.strip
+      puts ""
+      puts "Would you like to find out more about another athlete?"
+    else 
+      puts "" 
+    end 
+  end     
 
 
-     puts ""
-     
-    puts "Would you like to find out more about another athlete?"
-    
-  
-  end 
 
 
 end 
--- lib/top_athletes/scraper.rb
+++ lib/top_athletes/scraper.rb
@@ -12,15 +12,71 @@ def self.scrape_athletes
 
 def self.scrape_description 
  page = Nokogiri::HTML(open("https://www.businessinsider.com/iconic-female-athletes-women-sports-2019-7?r=US&IR=T"))
-
-
- #The issue is in the CSS element I have scraped 
  page.css("div.slide-layout.clearfix p").each.with_index do |description, index|
-    description = page.css("div.slide-layout.clearfix p")[index].text
+ #first_athlete = 
+#description = page.css("div.slide-layout.clearfix p")[index].text
+#first_description = page.css("div.slide-layout.clearfix p")[0..2].text.strip 
  end 
- 
+
+ #if user input = 1 puts wilma etc.
+ #first_athlete = page.css("div.slide-layout.clearfix p")[0..2].text
+ # 2billie = page.css("div.slide-layout.clearfix p")[3..5].text
+ # 3vonn =  page.css("div.slide-layout.clearfix p")[6..9].text
+ # 4raisman  = page.css("div.slide-layout.clearfix p")[10..11].text
+ # 5morgan = page.css("div.slide-layout.clearfix p")[12..15].text
+ # 6liukin  = page.css("div.slide-layout.clearfix p")[16..18].text
+ # 7serena = page.css("div.slide-layout.clearfix p")[19..23].text
+# 8 patrick = page.css("div.slide-layout.clearfix p")[24..27].text
+#9 rousey = age.css("div.slide-layout.clearfix p")[28..32].text
+
+#10 sharapova = page.css("div.slide-layout.clearfix p")[33..35].text
+#11 ledecky = page.css("div.slide-layout.clearfix p")[36..38].text
+# 12 simone biles = page.css("div.slide-layout.clearfix p")[39..43].text
+#13 franklin = page.css("div.slide-layout.clearfix p")[44..47].text
+# 14 Rapinoe =  page.css("div.slide-layout.clearfix p")[48..51].text
+#15 graf = page.css("div.slide-layout.clearfix p")[52..56].text
+# 16 treanor n jennings = page.css("div.slide-layout.clearfix p")[57..60].text
+# 17 taurasi = page.css("div.slide-layout.clearfix p")[61..63].text
+#18 latynina = page.css("div.slide-layout.clearfix p")[64..65].text
+# 19 joyner kersee = page.css("div.slide-layout.clearfix p")[66..68].text
+#20 c parker = page.css("div.slide-layout.clearfix p")[69..72].text
+# 21 comaneci = page.css("div.slide-layout.clearfix p")[73..75].text
+## 22 Navratilova = page.css("div.slide-layout.clearfix p")[76..80].text
+# 23 da silva = page.css("div.slide-layout.clearfix p")[81..82].text
+# 24 thompson = page.css("div.slide-layout.clearfix p")[83..85].text
+# 25 lieberman = page.css("div.slide-layout.clearfix p")[86..89].text
+# 26 hamm = page.css("div.slide-layout.clearfix p")[90..93].text
+# 27 wambach =  page.css("div.slide-layout.clearfix p")[94..98].text
+# 28  retton = page.css("div.slide-layout.clearfix p")[99..101].text
+# 29 kwan = page.css("div.slide-layout.clearfix p")[102..103].text
+# 30 venus = page.css("div.slide-layout.clearfix p")[104..107].text
+# 31 Moore = page.css("div.slide-layout.clearfix p")[108..110].text
+# 32 zaharias = page.css("div.slide-layout.clearfix p")[111..115].text
+# 33 swoopes = page.css("div.slide-layout.clearfix p")[116..118].text
+#34 miller = page.css("div.slide-layout.clearfix p")[119..122].text
+# 35 lopez =page.css("div.slide-layout.clearfix p")[123..125].text
+# leslie = page.css("div.slide-layout.clearfix p")[126..127].text
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 end
 
 
  
  end 
+
+ #what I found to print certain athletes description page.css("div.slide-layout.clearfix p")[3..5].text
